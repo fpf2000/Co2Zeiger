e4e589fcbc4e1faaf92ab0f7e5c593c0b0aedc78
falk-peter fietz <falk@falkpetersiMac.fritz.box>
2020-11-27 17:19:39 +0100

    Code bereinigt

--- CO2_LED_Zeiger0024_oWifi/CO2_LED_Zeiger0024_oWifi.ino
+++ CO2_LED_Zeiger0024_oWifi/CO2_LED_Zeiger0024_oWifi.ino
@@ -19,14 +19,9 @@
 
 #include <Servo.h>
 #include <FastLED.h>
-//#include <Adafruit_NeoPixel.h>
 #include <Wire.h>
-//#include <ESP8266WiFi.h>
 #include <ArduinoOTA.h>
 #include <DNSServer.h>
-//#include <ESP8266WebServer.h>
-//#include "dep/pubsubclient-2.7/src/PubSubClient.cpp"
-//#include "dep/WiFiManager-0.15.0/WiFiManager.cpp"
 
 #include "SparkFun_SCD30_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_SCD30
 SCD30 airSensor;
@@ -47,22 +42,6 @@ int   mapvalue = 0;
 
 int CO2 = 0 ;
 
-//#include "settings.h"
-
-/*#ifdef USE_HA_AUTODISCOVERY
-  #define FIRMWARE_PREFIX "esp8266-co2Zeiger"
-  char MQTT_TOPIC_LAST_WILL[128];
-  char MQTT_TOPIC_CO2_MEASUREMENT[128];
-  char MQTT_TOPIC_TEMPERATURE_MEASUREMENT[128];
-#endif
-
-WiFiClient wifiClient;
-PubSubClient mqttClient;
-
-uint8_t mqttRetryCounter = 0;
-
-char hostname[16];
-*/
 Servo myservo_2;
 
 
@@ -92,48 +71,6 @@ void setup()
  //The SCD30 has data ready every two seconds
 delay(6000);
 
- /*WiFiManager wifiManager;
-  int32_t chipid = ESP.getChipId();
-
-  Serial.print("MQTT_MAX_PACKET_SIZE: ");
-  Serial.println(MQTT_MAX_PACKET_SIZE);
-
-#ifdef HOSTNAME
-  hostname = HOSTNAME;
-#else
-  snprintf(hostname, 24, "CO2ZEIGER-%X", chipid);
-#endif
-*/
-  
-/*
-#ifdef USE_HA_AUTODISCOVERY
-  snprintf(MQTT_TOPIC_LAST_WILL, 127, "%s/%s/presence", FIRMWARE_PREFIX, hostname);
-  snprintf(MQTT_TOPIC_CO2_MEASUREMENT, 127, "%s/%s/%s_%s/state"(airSensor.getCO2(), FIRMWARE_PREFIX, hostname, hostname, "co2");
-  snprintf(MQTT_TOPIC_TEMPERATURE_MEASUREMENT, 127, "%s/%s/%s_%s/state", FIRMWARE_PREFIX, hostname, hostname, "temp");
-#endif
-
-wifiManager.setTimeout(120);
-
-#ifdef CONF_WIFI_PASSWORD
-  wifiManager.autoConnect(hostname, CONF_WIFI_PASSWORD);
-#else
-  wifiManager.autoConnect(hostname);
-#endif
-
-if(!wifiManager.autoConnect(hostname)) {
-    Serial.println("failed to connect and hit timeout");
-    redKreis();
-    delay(100);
-    redKreis();
-    delay(100);
-    redKreis();
-    delay(100);
-    //Starte Zeiger
-    zeiGer();
-    //Starte Anzeige
-    ledSCD30();
-  }
-  */
   myservo_2.attach(2);
   Serial.println("\n");
   Serial.print("Servo_auf_0 ");
@@ -147,23 +84,11 @@ if(!wifiManager.autoConnect(hostname)) {
 
   
   delay(2000);
-  /*
-  WiFi.hostname(hostname);
-  mqttClient.setClient(wifiClient);
-  mqttClient.setServer(MQTT_HOST, 1883);
 
-  Serial.print("Hostname: ");
-  Serial.println(hostname);
-  */
-  
   zeiGer();
   delay(500);
   ledSCD30();
   delay(2000);
-  //mqttConnect();
-  //delay(2000);
-  
-
  
 }
 void zeiGer(){
@@ -189,53 +114,7 @@ void redKreis(){
     delay(500);
   }  
 }
-/*
-void mqttConnect() {
-  while (!mqttClient.connected()) {
 
-    bool mqttConnected = false;
-    if (MQTT_USERNAME && MQTT_PASSWORD) {
-      mqttConnected = mqttClient.connect(hostname, MQTT_USERNAME, MQTT_PASSWORD, MQTT_TOPIC_LAST_WILL, 1, true, MQTT_LAST_WILL_PAYLOAD_DISCONNECTED);
-    } else {
-      mqttConnected = mqttClient.connect(hostname, MQTT_TOPIC_LAST_WILL, 1, true, MQTT_LAST_WILL_PAYLOAD_DISCONNECTED);
-    }
-
-    if (mqttConnected) {
-      Serial.println("Connected to MQTT Broker");
-      mqttClient.publish(MQTT_TOPIC_LAST_WILL, MQTT_LAST_WILL_PAYLOAD_CONNECTED, true);
-      mqttRetryCounter = 0;
-    } else {
-      Serial.println("Failed to connect to MQTT Broker");
-      ledSCD30();
-      if (mqttRetryCounter++ > MQTT_MAX_CONNECT_RETRY) {
-        Serial.println("Restarting uC");
-        ESP.restart();
-      }
-
-      delay(2000);
-    }
-  }
-}
-
-
-void mqttPublish(){
-    String cO2=String("co2: "+String(String(airSensor.getCO2())));
-    mqttClient.publish(hostname,cO2.c_str());
-    delay(1000);
-    String tMp=String("temp: "+String(String(airSensor.getTemperature())));
-    mqttClient.publish(hostname,tMp.c_str());
-    delay(1000);
-    String hUm=String("hum(%): "+String(String(airSensor.getHumidity())));
-    mqttClient.publish(hostname,hUm.c_str());
-    delay(1000);
-    Serial.print("\nmqtt publish: "); 
-    Serial.print(cO2);
-    Serial.print("\nmqtt publish: ");
-    Serial.print(tMp);
-    Serial.print("\nmqtt publish: ");
-    Serial.print(hUm);
-  }
-*/
 void ledSCD30(){
 if (airSensor.dataAvailable())
   {
@@ -333,7 +212,4 @@ void loop()
 
   delay(500);
   
-  //mqttPublish();
-  
-  //delay(6000);
 }
